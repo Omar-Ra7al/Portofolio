eb4fe99f899257d57aaf23e4d804987e4ad5e60f
OMAR-RAHAL <omar.ra7al@icloud.com>
2024-10-17 00:40:59 +0300

    hanndel images size & add lazy loading for un neccessaery img in home

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -11,11 +11,12 @@ import CardComponent from "../Card/Card";
 
 // import Education from "../../assets/imgs/The-Biggest-Education-Trends-Of-The-Next-10-Years.webp";
 // import Education from "../../assets/imgs/opengraph.png";
-import Education from "../../assets/imgs/opengraph_11zon.webp";
 // import Education from "../../assets/imgs/1616685010519.png";
 // import Education from "../../assets/imgs/1616685010519 (1)_11zon (1).png";
 // import Education from "../../assets/imgs/abstract-graduation-cap-on-laptop-600nw-2292894983-transformed_11zon (1).webp";
+// import Education from "../../assets/imgs/2.webp";
 
+import Education from "../../assets/imgs/1.webp";
 import TimeZone from "../../assets/imgs/TimeZone.webp";
 import Client from "../../assets/imgs/Client.webp";
 export default function AboutMe() {
