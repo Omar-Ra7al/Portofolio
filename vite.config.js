41de9088a63b6e3a71ad0bf4ecf408726ca1aa76
OMAR-RAHAL <omar.ra7al@icloud.com>
2024-10-16 22:47:32 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Use 'esbuild' for faster minification or 'terser' for better compression
-    minify: "terser", // Use 'esbuild' if you want faster build times
-  },
-});
+})
